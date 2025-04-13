efba1611b9aa45bb5ef2733e163d4a0ce8a7f40b
AhmadShirani <ah1sh2022@gmail.com>
2025-04-13 16:52:25 +0330

    update

--- pages/index.vue
+++ pages/index.vue
@@ -1,78 +1,66 @@
 <template>
-    <div class="p-5 w-screen h-screen flex flex-col gap-7 items-center justify-center">
+    <div class="p-5 w-screen h-screen flex flex-col gap-7 items-center justify-center bg-[#7462e1]">
 
 
-        <div class="relative overflow-x-auto">
-    <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 border">
-        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 border">
+        <div class="relative overflow-x-auto rounded-md">
+    <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 rounded shadow-xl/20">
+        <thead class=" text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
-                <th scope="col" class="px-6 py-3 border text-center">
+                <th scope="col" class="px-6 py-3 text-center">
                    نام و نام خانوادگی
                 </th>
-                <th scope="col" class="px-6 py-3 border text-center">
+                <th scope="col" class="px-6 py-3 text-center">
                     وضعیت
                 </th>
-                <th scope="col" class="px-6 py-3 border text-center">
+                <th scope="col" class="px-6 py-3 text-center">
                     ساعات حضور
                 </th>
-                <th scope="col" class="px-6 py-3 border text-center">
+                <th scope="col" class="px-6 py-3 text-center">
                     عملیات
                 </th>
             </tr>
         </thead>
         <tbody>
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
-                <th scope="row" class="border px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white text-center">
+            <tr class="bg-white">
+                <th id="name1" scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white text-center">
                    احمد شیرانی
                 </th>
-                   <!-- <NuxtLink class="block" to="/time-login">
-                    <td class="px-10 py-4 border text-[#02312c] bg-[#00ff95] text-center w-full h-full">زمان ورود</td>
-                   </NuxtLink>
-                <NuxtLink class="block" to="/departure-time">
-                    <td class="px-10 py-4 border bg-[#df0000] text-white text-center">زمان خروج</td>
-                   </NuxtLink> -->
-                <td class="px-6 py-4 border text-center">
+                <td class="px-6 py-4 text-center">
                     
                 </td>
-                <td class="px-6 py-4 border text-center">
+                <td class="px-6 py-4 text-center">
                    
                 </td>
                     <NuxtLink class="" to="/time-login">
-                        <td class="border text-[#02312c] bg-[#00ff95] flex justify-center items-center w-24 h-14">ثبت زمان</td>
+                        <td class="text-white bg-[#7462e1] flex justify-center items-center w-24 h-14 border-1 border-dotted">ثبت زمان</td>
                    </NuxtLink>
             </tr>
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200 border">
-                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white border text-center">
+            <tr class="bg-white">
+                <th id="name-2" scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white text-center">
                     ستایش جعفری
                 </th>
-                <td class="px-6 py-4 border text-center">
+                <td class="px-6 py-4 text-center">
                     
                 </td>
-                <td class="px-6 py-4 border text-center">
+                <td class="px-6 py-4 text-center">
                    
                 </td>
                 <NuxtLink class="" to="/time-login">
-                        <td class="border text-[#02312c] bg-[#00ff95] flex justify-center items-center w-24 h-14">ثبت زمان</td>
+                        <td class="text-white bg-[#7462e1] flex justify-center items-center w-24 h-14">ثبت زمان</td>
                    </NuxtLink>
             </tr>
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200 border">
-                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white border">
+            <tr class="bg-white dark:bg-gray-800 dark:border-gray-700 border-gray-200">
+                <th id="name-3" scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                     محمدرضا زرگرزاده
                 </th>
-                <!-- <NuxtLink class="block" to="/time-login">
-                    <td class="px-10 py-4 border text-[#02312c] bg-[#00ff95] text-center w-full h-full">زمان ورود</td>
-                   </NuxtLink>
-                   <NuxtLink class="block" to="/departure-time">
-                    <td class="px-10 py-4 border bg-[#df0000] text-white text-center">زمان خروج</td>
-                   </NuxtLink> -->
-                <td class="px-6 py-4 border text-center">
+                <td class="px-6 py-4 text-center">
                    
                 </td>
-                <td class="px-6 py-4 border text-center">
+                <td class="px-6 py-4 text-center">
                    
                 </td>
                 <NuxtLink class="" to="/time-login">
-                        <td class="border text-[#02312c] bg-[#00ff95] flex justify-center items-center w-24 h-14">ثبت زمان</td>
+                        <td class="text-white bg-[#7462e1] flex justify-center items-center w-24 h-14">ثبت زمان</td>
                    </NuxtLink>
             </tr>
         </tbody>
--- pages/time-login.vue
+++ pages/time-login.vue
@@ -1,16 +1,25 @@
+<script>
+
+</script>
 <template>
-    <div class="p-5 w-screen h-screen flex flex-col gap-7 items-center justify-center">
-        <h1 class="font-bold text-2xl">زمان ورود</h1>
-        <a href=""></a>
-        <button class="rounded-full bg-black w-40 h-40"><h2 class="text-white text-4xl">GO</h2></button>
-        <h2 class="">تاریخ امروز</h2>
-        <div class="w-72 bg-black p-1 rounded">
-            <span class="mt-6 text-white flex items-center justify-center mb-6 text-2xl font-bold"></span>
+    <div class="fixed">
+        <div class="text-white w-screen h-screen flex flex-col gap-7 items-center justify-center bg-gray-100 ">
+        <div class="bg-[#7462e1] rounded-b-full h-3/4 w-7xl absolute top-0"></div>
+        <div class="z-10">
+            <h1 class="flex items-center justify-center font-bold text-2xl mb-11">زمان ورود</h1>
+        <button class="rounded-full bg-white w-40 h-40 mb-5 mr-20"><h2 class="justify-center text-black text-center text-4xl">ثبت</h2></button>
+        <div class="bg-white w-80 h-80 rounded-3xl text-black flex justify-center items-center flex-col">
+            <h2>تاریخ امروز</h2>
+            <div class="bg-[#7462e1] rounded mt-6 text-white w-64 h-10 flex items-center justify-center">
+                <p class="block text-center"></p>
+            </div>
+            <h2 class="mt-4">زمان</h2>
+            <div class="bg-[#7462e1] rounded mt-5 text-white w-64 h-10 flex items-center justify-center mb-5">
+                <p class="block text-center"></p>
+            </div>
+            <NuxtLink class="bg-[#7462e1] p-1 w-28 h-10 rounded mt-4 text-white text-center" to="/">خروج</NuxtLink> 
         </div>
-        <h2 class="">زمان ورود</h2>
-        <div class="w-72 bg-black p-1 rounded">
-            <span class="mt-6 text-white flex items-center justify-center mb-6 text-2xl font-bold"></span>
         </div>
-        <NuxtLink class="bg-black p-1 w-28 h-10 rounded mt-6 text-white text-center" to="/">خروج</NuxtLink>
+    </div>
     </div>
 </template>
